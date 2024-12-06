9aa826ec335607c6e97514071372f2318cc19f71
Shibam Mandal <shibammandal603@gmail.com>
2024-12-06 23:13:43 +0530

    deploy

--- flat_price_predictor/wsgi.py
+++ flat_price_predictor/wsgi.py
@@ -4,3 +4,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flat_price_predictor.settings')
 
 application = get_wsgi_application()
+
+app = application
